bff82d4b2ab8665a7d25e453cb485067df5cab6d
Abdalmohsen-H <egywebdes@gmail.com>
2023-08-01 16:44:18 -0700

    match exactly 10 digits phone number

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/^\d{10,10}/).join
-# regexp that match leading 10 digits phone numbers no delimeters in between
+puts ARGV[0].scan(/^\d{10,10}$/).join
+# regexp that match exactly  10 digits phone numbers no delimeters in between
