72ef042d89d890d37f351ee11f09177952e3ecbc
Abdalmohsen-H <egywebdes@gmail.com>
2023-08-01 17:48:31 -0700

    regexp for group starting with specific string and ending with specific ctring, advanced task

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,12 +1,13 @@
 #!/usr/bin/env ruby
 #another solution
-#from_starting_string = "[from:"
-#from_ending_string = "]"
-#puts ARGV[0].scan(/#{Regexp.escape(from_starting_string)}(.*?)#{Regexp.escape(from_ending_string)}/).join(',')
+#puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')
+#\s means match any white space char
 
 puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(',')
 
-#another solution
-#puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')
-
 # regexp that match sender, reciver, flags
+
+#another solution
+#from_starting_string = "[from:"
+#from_ending_string = "]"
+#puts ARGV[0].scan(/#{Regexp.escape(from_starting_string)}(.*?)#{Regexp.escape(from_ending_string)}/).join(',')
