74bdeae2c8a03f8094298860ae56d42c33987d9a
Abdalmohsen-H <egywebdes@gmail.com>
2023-09-26 18:54:23 -0700

    task 5: use puppet to install inginx with redirect on specific route

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,24 @@
+# puppet script to do tasks till task 3 with nginx installed
+exec {'install_nginx':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
+}
+
+package {'nginx':
+  ensure => 'present',
+}
+
+exec {'Hello_world':
+  command  => 'echo "Hello World!" > /var/www/html/index.html',
+  provider => shell,
+}
+
+exec {'replace_config_with_sed';
+  command  => 'sed -i "s|server_name _;|server_name _;\\n\trewrite ^/redirect_me https://gr8dev.tech/ permanent;|" /etc/nginx/sites-enabled/default':
+  provider => shell,
+}
+
+exec {'apply_new_config':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
