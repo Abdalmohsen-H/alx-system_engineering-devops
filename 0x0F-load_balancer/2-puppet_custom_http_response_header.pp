b834f49620af5ef27158af125ff1fc88a4677cd9
Abdalmohsen-H <egywebdes@gmail.com>
2023-10-02 15:28:06 -0700

    task 2: fix puppet to install & config Nginx like task 0

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,32 +1,16 @@
 # task2: puppet code instead of bash script on task 0, for Nginx install. and config
-#Puppet class to apply Nginx installation and config.
-class nginx_config {
-  package { 'nginx':
-    ensure  => installed,
-  }
-
-  file_line { 'add_http_header':
-    path  => '/etc/nginx/nginx.conf',
-    match => 'http {',
-    line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
-    }
-
-  exec {'run':
-    command => '/usr/sbin/service nginx restart',
-    }
-
-#   exec { 'ufw-allow-ssh':
-#     command => 'ufw allow 22/tcp',
-#     unless  => 'ufw status | grep 22/tcp | grep ALLOW',
-#   }
-
-#   exec { 'ufw-allow-http':
-#     command => 'ufw allow 80/tcp',
-#     unless  => 'ufw status | grep 80/tcp | grep ALLOW',
-#   }
+exec {'install_nginx':
+    command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+    provider => shell,
 }
 
-# Apply the class to the node
-node 'my_node_name' {
-  include nginx_config
+file_line { 'add_http_header':
+    path    => '/etc/nginx/nginx.conf',
+    match   => 'server_name _;',
+    line    => "server_name _;\n\tadd_header X-Served-By \'${hostname}\';",
+    require => Exec['install_nginx'],  # this line to specify the dependency
+}
+
+exec {'run':
+    command => '/usr/sbin/service nginx restart',
 }
