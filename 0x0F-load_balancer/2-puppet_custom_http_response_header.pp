3028f9109705eeda321f73f1e25ec5474724efc7
Abdalmohsen-H <egywebdes@gmail.com>
2023-10-02 15:51:29 -0700

    task 2: puppet for nginx header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,24 @@
 # task2: puppet code instead of bash script on task 0, for Nginx install. and config
 exec {'install_nginx':
-    command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
-    provider => shell,
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
 }
 
-file_line { 'add_http_header':
-    path    => '/etc/nginx/nginx.conf',
-    match   => 'server_name _;',
-    line    => "server_name _;\n\tadd_header X-Served-By \'${hostname}\';",
-    require => Exec['install_nginx'],  # this line to specify the dependency
+package {'nginx':
+  ensure => 'present',
 }
 
-exec {'run':
-    command => '/usr/sbin/service nginx restart',
+exec {'Hello_world':
+  command  => 'echo "Hello World!" > /var/www/html/index.html',
+  provider => shell,
+}
+
+exec {'replace_config_with_sed':
+  command  => 'sh -c "sed -i \"s|server_name _;|server_name _;\\n\\trewrite ^/redirect_me https://youtube.com permanent;\\n\\tadd_header X-Served-By \'${hostname}\';|\" /etc/nginx/sites-enabled/default"',
+  provider => shell,
+}
+
+exec {'apply_new_config':
+  command  => 'sudo service nginx restart',
+  provider => shell,
 }
