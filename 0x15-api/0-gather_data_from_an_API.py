646366f07e3873b22d55eca949756295fa612d38
Abdalmohsen-H <egywebdes@gmail.com>
2023-10-23 10:26:54 -0700

    task 0 : consume API,handle no arg condition

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,25 +15,26 @@ def mani_func():
     """ task 0 : consume API
     """
     # Make an API call  and store the response #
-    id = argv[1]
-    todosurl = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
-    todos_resp = requests.get(todosurl)
-    total_tasks = len(todos_resp.json())
-    done_tasks_contr = 0
-    done_tasks = []
-    for task in todos_resp.json():
-        if task['completed'] is True:
-            done_tasks_contr += 1
-            done_tasks.append(task['title'])
+    if len(argv) > 1:
+        id = argv[1]
+        todosurl = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
+        todos_resp = requests.get(todosurl)
+        total_tasks = len(todos_resp.json())
+        done_tasks_contr = 0
+        done_tasks = []
+        for task in todos_resp.json():
+            if task['completed'] is True:
+                done_tasks_contr += 1
+                done_tasks.append(task.get('title', None))
 
-    usersurl = f'https://jsonplaceholder.typicode.com/users/{id}'
-    usr_jsn_resp = requests.get(usersurl).json()
-    usr_name = usr_jsn_resp['name']
-    print("Employee {} is done with tasks({}/{}):".format(
-        usr_name, done_tasks_contr, total_tasks
-    ))
-    for task in done_tasks:
-        print(f"\t{task}")
+        usersurl = f'https://jsonplaceholder.typicode.com/users/{id}'
+        usr_jsn_resp = requests.get(usersurl).json()
+        usr_name = usr_jsn_resp.get('name', None)
+        print("Employee {} is done with tasks({}/{}):".format(
+            usr_name, done_tasks_contr, total_tasks
+        ))
+        for task in done_tasks:
+            print(f"\t{task}")
 
 
 if __name__ == '__main__':
