99f84178f703d937d0c0431a08ee358391042e83
Abdalmohsen-H <egywebdes@gmail.com>
2023-10-23 10:44:32 -0700

    task 1 : consume API, save tasks to json

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+consume dummy API and get useful data
+in a specific format
+ouput is csv , record examlpe
+"USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+"""
+import requests
+from sys import argv
+
+
+def mani_func():
+    """ task 0 : consume API
+    """
+    # Make an API call  and store the response #
+    if len(argv) > 1:
+        id = argv[1]
+        todosurl = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
+        todos_resp = requests.get(todosurl)
+
+        usersurl = f'https://jsonplaceholder.typicode.com/users/{id}'
+        usr_jsn_resp = requests.get(usersurl).json()
+        usr_name = str(usr_jsn_resp.get('name', None))
+
+        with open(f"{id}.csv", 'w') as file:
+            for task in todos_resp.json():
+                user_id = str(id)
+                task_status = str(task.get('completed', None))
+                title = str(task.get('title', None))
+                # print(f"{user_id}, {usr_name}, {task_status}, {title}")
+                file.write(
+                    f"{user_id}, {usr_name}, {task_status}, {title}\n")
+
+
+if __name__ == '__main__':
+    mani_func()
