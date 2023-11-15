978b90c646432b34ff12dcd9a46ce89af50d906d
Abdalmohsen Hesham <egywebdes@gmail.com>
2023-11-15 01:37:11 -0800

    task 0 increase inginx ulimit with puupet

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# Puppet code to make sure that nginxcould handle large number of requests
+# number will be less or equal max. number of file descriptors that a shell session or process can have, you could check with "ulimit -n" cmd
+exec { 'Higher Nginx ULIMIT':
+  command => 'sudo sed -i "s/15/4096/" /etc/default/nginx; sudo service nginx restart',
+  path    => ['/usr/bin', '/bin'],
+}
